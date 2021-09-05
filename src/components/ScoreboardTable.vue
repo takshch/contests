89f5c99922e6fd21f155b03e52ea29dbd6ce0b12
takshch <takshch089@gmail.com>
2021-09-06 04:15:13 +0530

    Adds skeleton loading

---
+++ src/components/ScoreboardTable.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="mx-auto">
+    <template v-if="isLoading === false">
+      <v-card>
+        <v-card-title>
+          <v-col
+            class="ml-auto my-auto"
+            cols="12"
+            sm="6"
+            md="3"
+          >
+            <v-row>
+              <v-text-field
+                label="Full name"
+                single-line
+                outlined
+                v-model="search"
+              >
+              </v-text-field>
+            </v-row>
+          </v-col>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="scores"
+          class="mb-9 v-example v-sheet v-sheet--outlined theme--light rounded"
+          :search="search"
+          :custom-filter="filterByName"
+        >
+          <template v-slot:item.score="{ item }">
+            <span>
+              <span
+                v-if="typeof item.score === 'number'"
+                class="fw-bold"
+              >
+                {{ item.score }}
+              </span>
+              <br>
+              <span v-if="typeof item.totalTime === 'string'" class="text-grey">
+                {{ item.totalTime }}
+              </span>
+            </span>
+          </template>
+          <template v-slot:item.name="{ item }">
+            {{ item.name }}
+          </template>
+          <template v-for="(slot, index) in slots" v-slot:[`item.${slot.value}`]="{ item }">
+            <ScoreItem
+              :key="index"
+              :score="item[slot.value].score"
+              :id="item[slot.value].id"
+              :submissionTime="item[slot.value].submissionTime"
+            >
+            </ScoreItem>
+          </template>
+        </v-data-table>
+      </v-card>
+    </template>
+    <vue-skeleton-loader
+      v-else
+      type="rect"
+      :width="300"
+      :height="50"
+      animation="wave"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import VueSkeletonLoader from 'skeleton-loader-vue';
+import ScoreItem from '@/components/ScoreItem.vue';
+
+export default Vue.extend({
+  components: {
+    VueSkeletonLoader,
+    ScoreItem,
+  },
+  props: ['scores', 'slots', 'headers', 'isLoading'],
+  data() {
+    return {
+      search: '',
+    };
+  },
+  methods: {
+    filterByName(value, search, item) {
+      return value != null && search != null
+      && typeof value === 'string' && value.toString().toLowerCase().indexOf(search) !== -1;
+    },
+  },
+});
+</script>
+
+<style>
+.fw-bold {
+  font-weight: bold !important;
+}
+.text-grey {
+  color: #9e9e9e !important;
+}
+.text-green {
+  color: #4caf50 !important;
+}
+.text-red {
+  color: #f44336 !important;
+}
+</style>
--- src/views/Scoreboard.vue
+++ src/views/Scoreboard.vue
@@ -1,18 +1,52 @@
 <template>
     <div class="flex">
-      <Score-board class="mx-auto mt-12" />
+      <ScoreboardTable class="mx-auto mt-12"
+        :scores="scores"
+        :slots="slots"
+        :headers="headers"
+        :isLoading="isLoading"
+      >
+      </ScoreboardTable>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Scoreboard from '@/components/Scoreboard.vue';
+import ScoreboardTable from '@/components/ScoreboardTable.vue';
 
 export default Vue.extend({
   name: 'Scoreboard',
-
   components: {
-    'Score-board': Scoreboard,
+    ScoreboardTable,
+  },
+  computed: {
+    scores() {
+      let { scores } = this.$store.getters.getScoreboard;
+      if (scores) {
+        scores = scores.map((score) => {
+          const newScore = Object.hasOwnProperty.call(score, 'problems') ? { ...score, ...score.problems } : {};
+          delete newScore.problems;
+          return newScore;
+        });
+      }
+      console.log('scores', scores);
+      return scores;
+    },
+    slots() {
+      const { headers: { problems } } = this.$store.getters.getScoreboard;
+      return problems;
+    },
+    headers() {
+      const scoreboard = this.$store.getters.getScoreboard;
+      let headers = scoreboard?.headers?.main && scoreboard?.headers?.problems
+        ? [...scoreboard.headers.main, ...scoreboard.headers.problems] : [];
+      console.log(headers);
+      headers = headers.map((header) => ({ ...header, align: 'center' }));
+      return headers;
+    },
+    isLoading() {
+      return this.$store.getters.getLoadingStatus;
+    },
   },
   mounted() {
     this.$store.dispatch('setScoreboard');
